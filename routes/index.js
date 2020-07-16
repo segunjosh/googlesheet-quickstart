60ff12efd1080e84b01028ed2a0171bf3a8a787b
Olusegun Komolafe <joshusegun@gmail.com>
2020-07-16 23:53:53 +0100

    Major Changes

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,12 @@ const {
   findByVendorAndLocation,
 } = require("../controllers");
 
+router.get("/", (req, res) => {
+  return res.status(200).json({
+    status: 200,
+    message: "Welcome to the best API",
+  });
+});
 router.get("/getRecords", listRecords);
 router.get(
   "/getPriceByLocationAndSize/:location/:size/price",
